91338bb37e1c5305b2e2ea36e93045b13a36c216
thestraded <thestraded@gmail.com>
2018-02-07 18:51:14 -0500

    trying to work with redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { createStore } from 'redux'
+import { connect } from 'react-redux'
 
-
-function formatName(user){
-  return user.firstName + " " + user.lastName;
+class SecondReader extends React.Component {
+  constructor(props) {
+    super(props);
+    {console.log(props)}
+  }
+  render(){
+    return(
+      <h1>hello</h1>
+    )
+  }
 }
+
+
 
 class Input extends React.Component {
   constructor(props) {
@@ -18,7 +29,7 @@ class Input extends React.Component {
   userInput(event) {
     this.setState({username: event.target.value});
   }
-  
+
   passInput(event){
     this.setState({password: event.target.value});
   }
@@ -45,10 +56,66 @@ class Input extends React.Component {
 
 
 const element = (
-  <Input />
+  [<Input />,
+  <SecondReader />]
 )
 
 ReactDOM.render(
   element,
   document.getElementById('root')
 );
+
+
+
+
+
+const mapStateToProps = store => {
+  return {
+    counter: store.counter
+  }
+}
+
+/**
+ * This is a reducer, a pure function with (state, action) => state signature.
+ * It describes how an action transforms the state into the next state.
+ *
+ * The shape of the state is up to you: it can be a primitive, an array, an object,
+ * or even an Immutable.js data structure. The only important part is that you should
+ * not mutate the state object, but return a new object if the state changes.
+ *
+ * In this example, we use a `switch` statement and strings, but you can use a helper that
+ * follows a different convention (such as function maps) if it makes sense for your
+ * project.
+ */
+function counter(state = 0, action) {
+  switch (action.type) {
+  case 'INCREMENT':
+    return state + 1
+  case 'DECREMENT':
+    return state - 1
+  default:
+    return state
+  }
+}
+
+// Create a Redux store holding the state of your app.
+// Its API is { subscribe, dispatch, getState }.
+let store = createStore(counter)
+
+// You can use subscribe() to update the UI in response to state changes.
+// Normally you'd use a view binding library (e.g. React Redux) rather than subscribe() directly.
+// However it can also be handy to persist the current state in the localStorage.
+
+store.subscribe(() =>
+  console.log(store.getState())
+)
+
+// The only way to mutate the internal state is to dispatch an action.
+// The actions can be serialized, logged or stored and later replayed.
+store.dispatch({ type: 'INCREMENT' })
+// 1
+store.dispatch({ type: 'INCREMENT' })
+// 2
+store.dispatch({ type: 'DECREMENT' })
+// 1 
+export default connect(mapStateToProps)(Input)
